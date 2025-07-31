69519f2251488ba1357e58aedd88d83f77763cb2
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-07-31 15:05:23 -0300

    test: método get para buscar um dispositivo especifico

---
+++ cypress/e2e/get-api.cy.js
@@ -0,0 +1,22 @@
+describe('Buscar dispositivos', () => {
+    
+
+    it('Buscar um dispositivo especifico', () => {
+        const get_id = '7'
+
+        cy.request({
+            method: 'GET',
+            url: `https://api.restful-api.dev/objects/${get_id}`,   
+            failOnStatusCode: false
+
+        }).then((resp) => {
+            expect(resp.status).equal(200)
+            expect(resp.body).not.empty
+            expect(resp.body.id).equal(get_id)
+
+            expect(resp.body.name).equal('Apple MacBook Pro 16')
+            expect(resp.body.data['CPU model']).equal('Intel Core i9')
+        })
+        
+    });
+});
