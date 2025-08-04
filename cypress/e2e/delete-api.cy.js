032a79a73bab216b58d578ae2bb35a0df9d86eed
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-08-04 20:09:11 -0300

    test: tenta deletar um dispositivo que não existe

--- cypress/e2e/delete-api.cy.js
+++ cypress/e2e/delete-api.cy.js
@@ -1,12 +1,12 @@
+import { faker } from '@faker-js/faker';
+
 describe('Deletar dispositivos', () => {
 
-    it('Deletar um dispositivo', () => {
-        
+    it('Deleta um dispositivo', () => {
         cy.cadastra_dispositivo().as('postDevice')
 
         cy.get('@postDevice').then((resp_post) => {
             expect(resp_post.status).equal(200)
-            
 
             cy.request({
                 method: 'DELETE',
@@ -21,4 +21,22 @@ describe('Deletar dispositivos', () => {
             })
         })
     });
+
+    it('Deleta um dispositivo que não existe', () => {
+        const id = faker.number.int({ min: 20, max: 999 })
+
+        cy.request({
+            method: 'DELETE',
+            url: `https://api.restful-api.dev/objects/${id}`,
+            failOnStatusCode: false,
+
+        }).then((resp_delete) => {
+            expect(resp_delete.status).equal(404)
+            expect(resp_delete.body.error)
+                .equal(`Object with id = ${id} doesn't exist.`)
+
+        })
+    })
+
+    
 });
